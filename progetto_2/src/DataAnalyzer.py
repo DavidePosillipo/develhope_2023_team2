78972f7f91b664a14971f1b129be4342ab030d05
Carlo Caldura <carlo.caldura@gmail.com>
2023-04-04 20:20:24 +0200

    Implementing the right path to access files from repo

--- progetto_2/src/DataAnalyzer.py
+++ progetto_2/src/DataAnalyzer.py
@@ -1,12 +1,20 @@
 from afinn import Afinn
 import pandas as pd
+import os
 
+# ottieni il percorso della directory contenente il file corrente
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+# costruisci il percorso completo al file di dati
+data_file_path_p = os.path.join(curr_dir, "..", "database", "raw", "p.xlsx")
+data_file_path_n = os.path.join(curr_dir, "..", "database", "raw", "n.xlsx")
 #import the sentiment words files into dataframes
-pos_words = pd.read_excel(r"C:\Users\Crypto.gunner\Desktop\CODING\DevelHope\Progetto_APP\develhope_2023_team2\progetto_2\database\raw\p.xlsx")['abound'].tolist()
-neg_words = pd.read_excel(r"C:\Users\Crypto.gunner\Desktop\CODING\DevelHope\Progetto_APP\develhope_2023_team2\progetto_2\database\raw\n.xlsx")['faced'].tolist()
+pos_words = pd.read_excel(data_file_path_p)['abound'].tolist()
+neg_words = pd.read_excel(data_file_path_n)['faced'].tolist()
 
 #import the google users reviews file into a dataframe
-df = pd.read_csv(r'C:\Users\Crypto.gunner\Desktop\CODING\DevelHope\Progetto_APP\develhope_2023_team2\progetto_2\database\raw\googleplaystore_user_reviews.csv')
+data_file_path_reviews = os.path.join(curr_dir, "..", "database", "raw", "googleplaystore_user_reviews.csv")
+df = pd.read_csv(data_file_path_reviews)
 afinn = Afinn()
 
 score_list = []

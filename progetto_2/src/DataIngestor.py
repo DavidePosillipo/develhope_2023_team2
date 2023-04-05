920d25fa494baa253a22400879e06cc9fe351820
Carlo Caldura <carlo.caldura@gmail.com>
2023-04-05 02:03:33 +0200

    fix merge divergence

--- progetto_2/src/DataIngestor.py
+++ progetto_2/src/DataIngestor.py
@@ -25,27 +25,6 @@ class DataIngestor:
         else:
             return 'Apoligies, but this format has not been implemented yet.'
         
-<<<<<<< HEAD
-'''save imageimport matplotlib.pyplot as plt
-import pandas as pd
-from pandas.table.plotting import table # EDIT: see deprecation warnings below
-
-ax = plt.subplot(111, frame_on=False) # no visible frame
-ax.xaxis.set_visible(False)  # hide the x axis
-ax.yaxis.set_visible(False)  # hide the y axis
-
-table(ax, df)  # where df is your data frame
-
-plt.savefig('mytable.png')
-###############
-load image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-image_path = "D:/foto's/fbshare.png"
-image = mpimg.imread(image_path)
-plt.imshow(image)
-plt.show()'''
-=======
     def load_to_list(self, path, col, format):
 
         if format == 'pickle':
@@ -60,4 +39,3 @@ plt.show()'''
         else:
             return 'Apoligies, but this format has not been implemented yet.'
         
->>>>>>> carlo

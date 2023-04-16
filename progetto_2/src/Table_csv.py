285505057156131c39a1bbe616308d8f7249cf5f
Carlo Caldura <carlo.caldura@gmail.com>
2023-04-16 17:32:42 +0200

    Working on table.csv as dbcreator class

--- progetto_2/src/Table_csv.py
+++ progetto_2/src/Table_csv.py
@@ -46,8 +46,29 @@ class db_handler():
             if self.conn is not None:
                 self.cur.close()
                 self.conn.close()
+    
+    
+    def insert_values_main(self, table_name, csv_path):
+        try:
+            # Connect to the PostgreSQL server
+            self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
+            # Open a cursor to perform database operations
+            self.cur = self.conn.cursor()
+            # Import data from CSV to table
+            with open(csv_path, 'r', encoding='utf-8-sig') as f:
+                next(f)
+                self.cur.copy_from(f, table_name, sep=',')
+            # Commit the transaction
+            self.conn.commit()
+            print("Table created and data imported successfully")
+        except psycopg2.Error as e:
+            print("Error creating table or importing data:", e)
+        finally:
+            if self.conn is not None:
+                self.cur.close()
+                self.conn.close()
 
-    def insert_data_from_csv(self, path, query):
+    def insert_values_categories(self, path, query):
         try:
             # Connect to the PostgreSQL server
             self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
@@ -59,8 +80,6 @@ class db_handler():
                 next(reader)  # skip the header row
                 for row in reader:
                     category = row[2]
-                    print(f"Row: {row}")
-                    print(f"Category: {category}")
                     self.cur.execute(query, (category, category))
             # Commit the transaction
             self.conn.commit()
@@ -72,15 +91,87 @@ class db_handler():
                 self.cur.close()
                 self.conn.close()
 
+    def insert_values_apps(self, path, query):
+        try:
+            # Connect to the PostgreSQL server
+            self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
+            # Open a cursor to perform database operations
+            self.cur = self.conn.cursor()
+            # Read in the CSV file and insert data into the apps table
+            with open(path, 'r', encoding='utf-8-sig') as f:
+                reader = csv.reader(f)
+                next(reader)  # skip the header row
+                for row in reader:
+                    App_ID, app_name, category, rating, reviews, size, installs, app_type, price, content_rating, genres, last_updated, age_restriction = row
+                    App_id_query = "SELECT App_ID FROM processed_googleplaystore WHERE Name = %s"
+                    category_id_query = "SELECT Category_ID FROM categories WHERE Name = %s"
+                    self.cur.execute(category_id_query, (category,))
+                    category_id = self.cur.fetchone()[0]
+                    app_values = (App_ID, app_name, category_id, rating, reviews, size, installs, app_type, price, content_rating, genres, last_updated, age_restriction)
+                    self.cur.execute(query, app_values)
+            # Commit the transaction
+            self.conn.commit()
+            print("Data inserted successfully")
+        except psycopg2.Error as e:
+            print("Error inserting data:", e)
+        finally:
+            if self.conn is not None:
+                self.cur.close()
+                self.conn.close()
+
+    
+    def test_query(self, table_name, limit=False):
+        try:
+            # Connect to the PostgreSQL server
+            conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
+            # Open a cursor to perform database operations
+            cur = conn.cursor()
+            # Execute a SELECT query on the table
+            if limit==True:
+                cur.execute(f"SELECT * FROM {table_name} LIMIT 10;")
+            else:
+                cur.execute(f"SELECT * FROM {table_name};")
+            rows = cur.fetchall()
+            # Print the rows returned by the query
+            for row in rows:
+                print(row)
+        except psycopg2.Error as e:
+            print("Error executing SELECT query:", e)
+        finally:
+            if conn is not None:
+                cur.close()
+                conn.close()
+
 db = db_handler('postgres', 'postgres', 'c', 'localhost', 'prova_db')
+
+table_query = """
+    CREATE TABLE processed_googleplaystore (
+    App_ID INT PRIMARY KEY,
+    App VARCHAR(256),
+    Category VARCHAR(50),
+    Rating VARCHAR(10),
+    Reviews VARCHAR(50),
+    Size VARCHAR(50),
+    Installs VARCHAR(50),
+    Type VARCHAR(10),
+    Price VARCHAR(50),
+    "Content Rating" VARCHAR(50),
+    Genres VARCHAR(50),
+    "Last Updated" VARCHAR(50),
+    "Age Restriction" VARCHAR(50)
+    )
+"""
+db.create_table(table_query)
+db.insert_values_main('processed_googleplaystore', './database/output/processed_googleplaystore.csv')
+db.test_query('processed_googleplaystore', True)
+
 table_query = """
     CREATE TABLE categories (
-        CategoryID SERIAL PRIMARY KEY,
+        Category_ID SERIAL PRIMARY KEY,
         Name VARCHAR(256) NOT NULL
     )
 """
 db.create_table(table_query)
-
 insert_query = """
     INSERT INTO categories (Name)
     SELECT %s
@@ -88,4 +179,29 @@ insert_query = """
         SELECT 1 FROM categories WHERE Name = %s
     )
 """
-db.insert_data_from_csv('./database/output/processed_googleplaystore.csv', insert_query)
+db.insert_values_categories('./database/output/processed_googleplaystore.csv', insert_query)
+db.test_query('categories')
+
+table_query = """
+    CREATE TABLE Apps (
+        App_ID INT REFERENCES processed_googleplaystore(App_ID),
+        AppName VARCHAR(256),
+        Category_ID INT REFERENCES categories(Category_ID),
+        Rating FLOAT(50),
+        Reviews INT,
+        Size INT,
+        Installs INT,
+        Type VARCHAR(15),
+        Price FLOAT,
+        Content_Rating VARCHAR(30),
+        Genres VARCHAR(50),
+        Last_Updated DATE,
+        Age_Restriction INT
+    )
+"""
+db.create_table(table_query)
+
+query = "INSERT INTO apps (Index, Name, Category_ID, Rating, Reviews, Size, Installs, Type, Price, ContentRating, Genres, LastUpdated, AgeRestriction) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+path = './database/output/processed_googleplaystore.csv'
+db.insert_values_apps(path, query)
+db.test_query('Apps', True)

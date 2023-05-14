d9d511a1bcd377ec83d2a29c3d9dd50c2da1b4e2
Carlo Caldura <carlo.caldura@gmail.com>
2023-05-14 11:41:55 +0200

    Doc strings DB_Handler

--- progetto_2/src/DB_Handler.py
+++ progetto_2/src/DB_Handler.py
@@ -2,25 +2,33 @@ import psycopg2
 import csv
 import pandas as pd
 
-class DB_Handler():
+class DB_Handler:
+    """A class for handling database operations."""
     def __init__(self, database, user, password, host, database_name):
+        """
+        Initialize the DB_Handler object.
+
+        Args:
+            database (str): The name of the default database.
+            user (str): The username for accessing the database.
+            password (str): The password for accessing the database.
+            host (str): The host address of the database server.
+            database_name (str): The name of the database to be created.
+
+        Raises:
+            psycopg2.Error: If there's an error creating the database.
+        """
         self.database = database
         self.user = user
         self.password = password
         self.host = host
         self.database_name = database_name
         try:
-            # Connect to the PostgreSQL server
             self.conn = psycopg2.connect(database=database, user=user, password=password, host=host)
-            # Open a cursor to perform database operations
             self.cur = self.conn.cursor()
-            # Rollback any open transaction
             self.cur.execute("ROLLBACK")
-            # Drop the database if it already exists
             self.cur.execute(f"DROP DATABASE IF EXISTS {database_name};")
-            # Execute a CREATE DATABASE command
             self.cur.execute(f"CREATE DATABASE {database_name};")
-            # Commit the transaction
             self.conn.commit()
             print("Database created successfully")
         except psycopg2.Error as e:
@@ -31,16 +39,21 @@ class DB_Handler():
                 self.conn.close()
 
     def create_table(self, query, table_name):
+        """
+        Create a table in the database.
+
+        Args:
+            query (str): The SQL query to create the table.
+            table_name (str): The name of the table to be created.
+
+        Raises:
+            psycopg2.Error: If there's an error creating the table.
+        """
         try:
-            # Connect to the PostgreSQL server and create a new database
             self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
-            # Open a cursor to perform database operations
             self.cur = self.conn.cursor()
-            # Drop the database if it already exists
             self.cur.execute(f"DROP TABLE IF EXISTS {table_name};")
-            # Execute the query to create the table
             self.cur.execute(query)
-            # Commit the transaction
             self.conn.commit()
             print("Table created successfully")
         except psycopg2.Error as e:
@@ -51,19 +64,25 @@ class DB_Handler():
                 self.conn.close()
 
     def insert_values_categories(self, path, query):
+        """
+        Insert values into the categories table from a CSV file.
+
+        Args:
+            path (str): The path to the CSV file containing the data.
+            query (str): The SQL query to insert values into the table.
+
+        Raises:
+            psycopg2.Error: If there's an error inserting the data.
+        """
         try:
-            # Connect to the PostgreSQL server
             self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
-            # Open a cursor to perform database operations
             self.cur = self.conn.cursor()
-            # Read in the CSV file and insert data into the categories table
             with open(path, 'r', encoding='utf-8-sig') as f:
                 reader = csv.reader(f)
-                next(reader)  # skip the header row
+                next(reader)  
                 for row in reader:
                     category = row[2]
                     self.cur.execute(query, (category, category))
-            # Commit the transaction
             self.conn.commit()
             print("Data inserted successfully")
         except psycopg2.Error as e:
@@ -74,20 +93,27 @@ class DB_Handler():
                 self.conn.close()
     
     def insert_values_apps(self, path, query):
+        """
+        Insert values into the apps table from a CSV file.
+
+        Args:
+            path (str): The path to the CSV file containing the data.
+            query (str): The SQL query to insert values into the table.
+
+        Raises:
+            psycopg2.Error: If there's an error inserting the data.
+        """
         try:
-            # Connect to the PostgreSQL server
             self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
-            # Open a cursor to perform database operations
             self.cur = self.conn.cursor()
             with open(path, 'r', encoding='utf-8-sig') as f:
                 reader = csv.reader(f)
-                next(reader)  # skip the header row
+                next(reader)  
                 num_rows = 0
                 for row in reader:
                     app_name = row[1]
                     self.cur.execute(query, (app_name, app_name))
                     num_rows += 1
-                # Commit the transaction
                 self.conn.commit()
                 print("Data inserted successfully")
         except psycopg2.Error as e:
@@ -99,15 +125,22 @@ class DB_Handler():
 
 
     def insert_values_main(self, path, query):
+        """
+        Insert values into the main table from a CSV file.
+
+        Args:
+            path (str): The path to the CSV file containing the data.
+            query (str): The SQL query to insert values into the table.
+
+        Raises:
+            psycopg2.Error: If there's an error inserting the data.
+        """
         try:
-            # Connect to the PostgreSQL server
             self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
-            # Open a cursor to perform database operations
             self.cur = self.conn.cursor()
-            # Read in the CSV file and insert data into the apps table
             with open(path, 'r', encoding='utf-8-sig') as f:
                 reader = csv.reader(f)
-                next(reader)  # skip the header row
+                next(reader)  
                 for row in reader:
                     Index, app_id, category_id, rating, reviews, size, installs, app_type, price, content_rating, genres, last_updated, age_restriction = row
                     app_id_query = """SELECT "App ID" FROM Apps WHERE "name" = %s"""
@@ -118,7 +151,6 @@ class DB_Handler():
                     app_id = self.cur.fetchone()[0]
                     app_values = (Index, app_id, category_id, rating, reviews, size, installs, app_type, price, content_rating, genres, last_updated, age_restriction)
                     self.cur.execute(query, app_values)
-            # Commit the transaction
             self.conn.commit()
             print("Data inserted successfully")
         except psycopg2.Error as e:
@@ -129,15 +161,22 @@ class DB_Handler():
                 self.conn.close()
 
     def insert_values_reviews(self, path, query):
+        """
+        Insert values into the reviews table from a CSV file.
+
+        Args:
+            path (str): The path to the CSV file containing the data.
+            query (str): The SQL query to insert values into the table.
+
+        Raises:
+            psycopg2.Error: If there's an error inserting the data.
+        """
         try:
-            # Connect to the PostgreSQL server
             self.conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
-            # Open a cursor to perform database operations
             self.cur = self.conn.cursor()
-            # Read in the CSV file and insert data into the apps table
             with open(path, 'r', encoding='utf-8-sig') as f:
                 reader = csv.reader(f)
-                next(reader)  # skip the header row
+                next(reader) 
                 for row in reader:
                     app_id, translated_review = row
                     app_id_query = """SELECT "App ID" FROM Apps WHERE "name" = %s"""
@@ -154,18 +193,26 @@ class DB_Handler():
                 self.conn.close()
 
     def read_table(self, table_name):
+        """
+        Read and retrieve data from a table.
+
+        Args:
+            table_name (str): The name of the table to read.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the retrieved data.
+
+        Raises:
+            psycopg2.Error: If there's an error executing the SELECT query.
+        """
         try:
-            # Connect to the PostgreSQL server
             conn = psycopg2.connect(database=self.database_name, user=self.user, password=self.password, host=self.host)
-            # Open a cursor to perform database operations
             cur = conn.cursor()
-            # Execute a SELECT query on the table
             cur.execute(f"SELECT * FROM {table_name} LIMIT 10;")
             data = cur.fetchall()
         except psycopg2.Error as e:
             print("Error executing SELECT query:", e)
 
-        # Create dataframe
         cols = []
         for elt in cur.description:
             cols.append(elt[0])

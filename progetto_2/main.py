8dcabd8e55b310fd666764094b97c73275122ee2
Carlo Caldura <carlo.caldura@gmail.com>
2023-05-14 16:42:22 +0200

    Update DB_Handler class implementing pipeline and cleaning main.py

--- progetto_2/main.py
+++ progetto_2/main.py
@@ -9,7 +9,7 @@ from src.DB_Handler import DB_Handler
 di = DataIngestor()
 dp = DataPreprocessor()
 dv = DataVisualizer(library="seaborn", style='darkgrid', show=False, save=True) 
-da = DataAnalyzer()  # Any list of words formatted in one column
+da = DataAnalyzer()
 db = DB_Handler(database = 'postgres', user = 'postgres', password='c', host='localhost', database_name = 'googleplaystore')
 
 # Uploads csv file containing data about Google Play Store apps
@@ -52,68 +52,7 @@ dv.pipeline(df, df_all)
 # Loads PNG graphs based on library
 di.load_image('png', library='seaborn')
 
-# CATEGORY TABLE
-table_query = """
-    CREATE TABLE categories (
-        "Category ID" SERIAL PRIMARY KEY,
-        Name VARCHAR(256) NOT NULL
-    )
-"""
-db.create_table(table_query, 'categories')
-insert_query = """
-    INSERT INTO categories (Name)
-    SELECT %s
-    WHERE NOT EXISTS (
-        SELECT 1 FROM categories WHERE Name = %s
-    )
-"""
-db.insert_values_categories('./database/output/processed_googleplaystore.csv', insert_query)
-# APP TABLE
-# Define the table creation query
-table_query = """
-    CREATE TABLE apps (
-        "App ID" SERIAL PRIMARY KEY,
-        name VARCHAR(256) NOT NULL
-    )
-"""
-# Create the table
-db.create_table(table_query, 'apps')
-# Define the query for inserting data into the table
-insert_query = """
-    INSERT INTO apps (name)
-    SELECT %s
-    WHERE NOT EXISTS (
-        SELECT 1 FROM apps WHERE name = %s
-    )
-"""
-# Insert data from CSV file into the table
-db.insert_values_apps('./database/output/processed_googleplaystore.csv', insert_query)
-
-# APPS TABLE
-table_query = """
-    CREATE TABLE Main (
-    "Index" INT,
-    "App ID" INT REFERENCES apps("App ID"),
-    "Category ID" INT REFERENCES categories("Category ID"),
-    Rating VARCHAR(10),
-    Reviews VARCHAR(50),
-    Size VARCHAR(50),
-    Installs VARCHAR(50),
-    Type VARCHAR(10),
-    Price VARCHAR(50),
-    "Content Rating" VARCHAR(50),
-    Genres VARCHAR(50),
-    "Last Updated" VARCHAR(50),
-    "Age Restriction" VARCHAR(50)
-    )
-"""
-db.create_table(table_query, 'Main')
-query = """INSERT INTO Main (
-                "Index", "App ID", "Category ID", Rating, Reviews, Size, Installs, Type, Price, 
-                "Content Rating", Genres, "Last Updated", "Age Restriction") 
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-path = './database/output/processed_googleplaystore.csv'
-db.insert_values_main(path, query)
+db.run_data_pipeline()
 
 #!!! PAOLO !!!
 df = db.read_table('Main') 
